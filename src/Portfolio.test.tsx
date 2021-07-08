33578de4d6ea7977a4b92cfbaaeb2723a72dd57b
thelookus <calvino.lucas@gmail.com>
2021-07-08 19:45:20 -0300

    Initial commit/react-template

---
+++ src/Portfolio.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Portfolio from './Portfolio';
+
+test('renders learn react link', () => {
+  render(<Portfolio />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
