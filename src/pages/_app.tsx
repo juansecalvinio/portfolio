f48b1d2ce80bedcd5f52961f7ce0dd1ca7c5fff0
Juanse <j.s.calvinio@gmail.com>
2024-10-16 19:34:26 -0300

    Agregamos framer-motion

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,24 @@
 import React from "react";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import { AnimatePresence, motion } from "framer-motion";
 
 import theme from "theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={1}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 2 }}
+        >
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
     </ChakraProvider>
   );
 }
