84be6a161c7a1316d7f52110cda26dae025b3959
Juanse <j.s.calvinio@gmail.com>
2025-05-01 13:07:42 -0300

    Skills refactoring

--- src/sections/Skills/index.tsx
+++ src/sections/Skills/index.tsx
@@ -1,48 +1,6 @@
-import { Box, Heading, Tag } from "@chakra-ui/react";
 import React from "react";
-
-const skills: { colorScheme: string; skill: string }[] = [
-  {
-    colorScheme: "yellow",
-    skill: "JS",
-  },
-  {
-    colorScheme: "blue",
-    skill: "TypeScript",
-  },
-  {
-    colorScheme: "blue",
-    skill: "React",
-  },
-  {
-    colorScheme: "gray",
-    skill: "Next",
-  },
-  {
-    colorScheme: "green",
-    skill: "Node",
-  },
-  {
-    colorScheme: "red",
-    skill: "NestJS",
-  },
-  {
-    colorScheme: "blue",
-    skill: "Go",
-  },
-  {
-    colorScheme: "blue",
-    skill: "PostgreSQL",
-  },
-  {
-    colorScheme: "green",
-    skill: "MongoDB",
-  },
-  {
-    colorScheme: "gray",
-    skill: "Git",
-  },
-];
+import { Box, Heading, Tag } from "@chakra-ui/react";
+import { skills } from "./skills";
 
 export const Skills = () => {
   return (
@@ -51,19 +9,10 @@ export const Skills = () => {
         Skills
       </Heading>
       <Box display={"flex"} flexWrap={"wrap"} gap={"0.5rem"} marginTop={"1rem"}>
-        {/* <Tag colorScheme="yellow">Javascript</Tag>
-        <Tag colorScheme="red">HTML</Tag>
-        <Tag colorScheme="purple">CSS</Tag>
-        <Tag colorScheme="blue">TypeScript</Tag>
-        <Tag colorScheme="blue">React</Tag>
-        <Tag colorScheme="gray">Next</Tag>
-        <Tag colorScheme="green">NodeJS</Tag>
-        <Tag colorScheme="red">NestJS</Tag>
-        <Tag colorScheme="yellow">SQL</Tag>
-        <Tag colorScheme="green">MongoDB</Tag>
-        <Tag colorScheme="gray">Git</Tag> */}
         {skills.map((skill) => (
-          <Tag colorScheme={skill.colorScheme}>{skill.skill}</Tag>
+          <Tag key={skill.skill} colorScheme={skill.colorScheme}>
+            {skill.skill}
+          </Tag>
         ))}
       </Box>
     </Box>
---
+++ src/sections/Skills/skills.ts
@@ -0,0 +1,42 @@
+export const skills: { colorScheme: string; skill: string }[] = [
+  {
+    colorScheme: "yellow",
+    skill: "JS",
+  },
+  {
+    colorScheme: "blue",
+    skill: "TypeScript",
+  },
+  {
+    colorScheme: "blue",
+    skill: "React",
+  },
+  {
+    colorScheme: "gray",
+    skill: "Next",
+  },
+  {
+    colorScheme: "green",
+    skill: "Node",
+  },
+  {
+    colorScheme: "red",
+    skill: "NestJS",
+  },
+  {
+    colorScheme: "blue",
+    skill: "Go",
+  },
+  {
+    colorScheme: "blue",
+    skill: "PostgreSQL",
+  },
+  {
+    colorScheme: "green",
+    skill: "MongoDB",
+  },
+  {
+    colorScheme: "gray",
+    skill: "Git",
+  },
+];
